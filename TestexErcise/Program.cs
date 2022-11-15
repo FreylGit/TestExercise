e07a977b86488e390dc52977d8b0d3994dc37c23
ANDREY <41635263+FreylGit@users.noreply.github.com>
2022-11-15 17:09:23 +0300

    Почистил проект от лишних файлов

--- TestexErcise/Program.cs
+++ TestexErcise/Program.cs
@@ -1,11 +1,10 @@
-using AutoMapper;
+
 using Microsoft.EntityFrameworkCore;
 using TestexErcise.Data;
-using TestExercise.Configuration;
+
 using TestExercise.Data.Repositories;
 using TestExercise.Data.Repositories.Interfaces;
 
-IMapper mapper = MappingConfig.RegisterMaps().CreateMapper();
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
@@ -13,7 +12,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(
     options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddTransient<IProviderRepository, ProviderRepository>();
 builder.Services.AddTransient<IOrderRepository, OrderRepository>();
-builder.Services.AddSingleton(mapper);
+
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 var app = builder.Build();
 
